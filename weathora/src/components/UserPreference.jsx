4b365a1d6952a6c73ede9b8da45ca18384829035
VISHWARAJ-G <rajgvishwa630@gmail.com>
2025-04-13 03:25:46 +0530

    Navbar and User Preference Pages Completed

--- weathora/src/components/UserPreference.jsx
+++ weathora/src/components/UserPreference.jsx
@@ -5,20 +5,18 @@ import SiteLogo from "./SiteLogo";
 
 function UserPreference({ setHasUser }) {
   return (
-    <div className="py-10 flex justify-between">
-      <div className="flex items-center pl-20">
-        <img src="image.png" alt="User" className="h-[32rem] rounded-full" />
-      </div>
+    <div className="py-5">
       <div>
-        <div className="pb-4 flex flex-col justify-center items-center gap-2">
-          <SiteLogo showText={false} />
+        <div className="flex flex-col gap-3 items-center justify-center">
+          <SiteLogo />
           <h1 className="text-4xl font-extrabold text-white">Weathora</h1>
         </div>
-        <h1 className="text-white text-center text-4xl">
-          User Preference Form
-        </h1>
-        <div className="flex justify-center items-center p-10">
-          <div className="bg-blue-200 bg-opacity-15 rounded-3xl p-10 flex justify-center items-center">
+
+        <div className="flex justify-center items-center p-5">
+          <div className="bg-blue-200 bg-opacity-15 rounded-3xl p-10 flex flex-col gap-10 justify-center items-center">
+            <h1 className="text-white text-center text-4xl font-semibold">
+              User Preference Form
+            </h1>
             <PreferenceForm setHasUser={setHasUser} />
           </div>
         </div>
